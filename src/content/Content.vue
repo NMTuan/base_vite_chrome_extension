6d7a85b7f380852807beb606aaaee09ba982bf0d
NMTuan <nmtuan@qq.com>
2022-12-01 10:46:44 +0800

    chore: 优化样式

--- src/content/Content.vue
+++ src/content/Content.vue
@@ -2,17 +2,17 @@
  * @Author: NMTuan
  * @Email: NMTuan@qq.com
  * @Date: 2022-11-22 17:17:10
- * @LastEditTime: 2022-11-24 17:02:07
+ * @LastEditTime: 2022-12-01 10:43:29
  * @LastEditors: NMTuan
  * @Description: 
  * @FilePath: \base_vite_chrome_extension\src\content\Content.vue
 -->
 <template>
     <div class="
-    absolute inset-0 z-10 
+    fixed inset-0 z-[9999]
     bg-sky-800/50 backdrop-blur-sm 
     flex flex-col items-center justify-center 
-    text-4xl text-white leading-[64px]
+    text-[36px] text-white leading-[64px]
     ">
     <p class="font-bold">演示</p>
     <p>content-scripts</p>
